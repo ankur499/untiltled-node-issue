60b0f1afea3b4f55e32da630af79cce3126ae177
Ankur <xbbn320@eagleinvsys.com>
2020-07-01 15:48:03 +0530

    node loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const webpack = require('webpack')
 
 module.exports = {
   target: "node",
-  mode: "development",
   node: {
     __dirname: false,
   },
@@ -15,14 +14,6 @@ module.exports = {
     ]
   },
   resolve: {
-    extensions: ['.ts', '.js', '.json','.node','.mjs']
-  },
-  devtool: false,
-  plugins: [
-    new webpack.SourceMapDevToolPlugin({
-      filename: '[file].map[query]'
-    }),
-
-    new webpack.HotModuleReplacementPlugin() // !!!!!!! remove this line, the warning will not display
-  ]
+    extensions: ['.node']
+  }
 };
